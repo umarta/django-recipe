9c07c5131300f61346d4c6ca19ec8df18887c424
umarta <rahmad@umarta.dev>
2020-03-08 14:43:23 +0700

    add admin user page

--- app/core/test/test_admin.py
+++ app/core/test/test_admin.py
@@ -25,3 +25,16 @@ class AdminSiteTest(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """test user edit page work"""
+
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """test create user page"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
