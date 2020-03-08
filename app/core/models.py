77b7e27894d596595b069437b8ec34c14a760ab6
umarta <rahmad@umarta.dev>
2020-03-08 14:17:00 +0700

    fix missing save change on create superuser

--- app/core/models.py
+++ app/core/models.py
@@ -11,7 +11,6 @@ class UserManager(BaseUserManager):
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self.db)
-
         return user
 
     def create_superuser(self, email, password):
@@ -19,7 +18,10 @@ class UserManager(BaseUserManager):
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
-        return  user
+        user.save(using=self._db)
+
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User model email instead of username"""
